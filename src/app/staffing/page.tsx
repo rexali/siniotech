6fa9db3cf8a0b3606b9564a83225f45bca28afde
Aliyu Bello <aliyubababello@gmail.com>
2024-11-03 22:26:00 +0100

    staffing page updated

--- src/app/staffing/page.tsx
+++ src/app/staffing/page.tsx
@@ -46,11 +46,14 @@ export default function RecruiterPage() {
                         <div className='col-6'>
                             You will be able to:
                             <ul className="list-group list-group-flush">
-                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> Save money without hiring web designer or developer.</li>
-                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> Get your do-It-Yourself business website.</li>
-                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> Turn your website visitors to leads & customers.</li>
-                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> 1000 copies already downloaded.</li>
-                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> End soon</li>
+
+                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> You do not need to pay anything upfront to get the right talent for you.</li>
+                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> You only pay when you start enjoying the service of the talent.
+                                </li>
+                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> You can discontinue with our services at anytime without spending a dime</li>
+                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> Your talent will get free training from us </li>
+                                <li className="list-group-item bg-success text-justify"><i className="fa fa-star"></i> You will get free content on how to start your to recruit with high velocity </li>
+
                             </ul>
                         </div>
                         <div className='col-6'>
@@ -67,15 +70,15 @@ export default function RecruiterPage() {
                 <p>Dear Business Owner,</p>
                 <p>I am Aliyu Bello, a recruiter and digital Marketer.</p>
                 <p>
-                    Let is face it, recruiting talent on your own is frustrating at best. If you would rather spend that spend that time with friends and family, let our firm handle your talent recruitment or staffing this year for you.
+                    Let us face it, recruiting talent on your own is frustrating at best. If you would rather spend that spend that time with friends and family, let our firm handle your talent recruitment or staffing this year for you.
                 </p>
 
-                <p> Yeah! I have also suffered from this same problem before but today It's no more.</p>
+                <p> Yeah! I have also suffered from this same problem before but today It is no more.</p>
 
 
-                <p> I have worked hard day and night to solve this one problem and I believe you don't have to go through the same suffering and pain I had gone through.</p>
+                <p> I have worked hard day and night to solve this one problem and I believe you do not have to go through the same suffering and pain I had gone through.</p>
 
-                <p>Alas! There is a solution to your problem which is "online recruiting system" to get the right talent for your business.</p>
+                <p>Alas! There is a solution to your problem which is an online recruiting system to get the right talent for your business.</p>
 
                 <p>
                     All you need to do is to get us the job description or requirements and we will handle the rest for using our system.
@@ -95,12 +98,12 @@ export default function RecruiterPage() {
                 <p> You will be able to:</p>
 
                 <ul>
-                    <li>  You don't need to pay anything upfront to get the right talent for you.</li>
+                    <li>  You do not need to pay anything upfront to get the right talent for you.</li>
                     <li> You only pay when you start enjoying the service of the talent.
                     </li>
                     <li> You can discontinue with our services at anytime without spending a dime</li>
                     <li> Your talent will get free training from us </li>
-                    <li> You'll get free content on how to start your to recruit with high velocity </li>
+                    <li> You will get free content on how to start your to recruit with high velocity </li>
                 </ul>
                 <p>Here is a link to show you how your website will look like after you have edited and uploaded it to your server. View it here: https://siniotech.com</p>
 
@@ -112,7 +115,7 @@ export default function RecruiterPage() {
                 </ul>
 
                 <p>Get your free talent today and Pay only if you are satisfied with his/her services</p>
-                <p>Here is what you'll get if you take this offer now (use table):</p>
+                <p>Here is what you will get if you take this offer now (use table):</p>
 
                 <ul>
                     <li>One-time Fee: N100,500</li>
@@ -128,7 +131,7 @@ export default function RecruiterPage() {
 
 
 
-                <p>Right now you can have all these today set up for you by paying N95,000 only. Are you surprised? Don't be. Yes I mean it.</p>
+                <p>Right now you can have all these today set up for you by paying N95,000 only. Are you surprised? Do not be. Yes I mean it.</p>
 
                 <p>Register by sending a job description and requirements to us. Take this offer now. We are taking only 3 prospects so that we can render effective service to them. So be the first.
                     Think of how much it will cost you if you do not take action on this problem.</p>
