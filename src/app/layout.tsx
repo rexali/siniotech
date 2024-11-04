e4388ee372a6e842a5af5e69b61d85989fede05b
Aliyu Bello <aliyubababello@gmail.com>
2024-11-04 07:07:21 +0100

    logo and forms  added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <title>Digital Marketing, Sales, Advertising, Web Developer, Mobile App Developer</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
-        <link rel="icon" type="image/jpg" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/images/siniotech.png" />
       </head>
       <body className={inter.className}>
         {children}
---
+++ src/app/staffing/seekers/apply/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+export default function RecruiterPage() {
+
+    return (
+        <main>
+            <div className="container text-black mt-4">
+                <nav className="navbar navbar-expand-md navbar-light fixed-top" style={{ backgroundColor: "green" }} id="navbartop">
+                    <a className="navbar-brand" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span className="fa fa-bars d-md-none" style={{ fontSize: "medium" }}></span>
+                    </a>
+                    <a className="navbar-brand" href="/">Siniotech</a>
+                    <a className="navbar-brand  d-md-none" href="tel:+2347016807004"><i className="fa fa-phone"></i></a>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" style={{ color: 'white' }} href="#about">ABOUT</a>
+                            </li>
+                            <li className="nav-item"><a style={{ color: 'white' }} className="nav-link" href="#service">SERVICES</a></li>
+                            <li className="nav-item"><a style={{ color: 'white' }} className="nav-link" href="https://mujaware.com/blog">BLOG</a></li>
+                            <li className="nav-item"><a style={{ color: 'white' }} className="nav-link" href="#portfolio">PORTFOLIO</a></li>
+                            <li className="nav-item"><a style={{ color: 'white' }} className="nav-link" href="#pricing">PRICING</a></li>
+                            <li className="nav-item"><a style={{ color: 'white' }} className="nav-link" href="#training">TRAINING</a></li>
+                            <li className="nav-item"><a style={{ color: 'white' }} className="nav-link" href="#contact">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </nav><br /><br />
+                <nav className="nav-tabs d-md-none scrollmenu">
+                    <a className="nav-link" href="#about">ABOUT</a>
+                    <a className="nav-link" href="#service">SERVICES</a>
+                    <a className="nav-link" href="#portfolio">PORTFOLIO</a>
+                    <a className="nav-link" href="#pricing">PRICING</a>
+                    <a className="nav-link" href="#training">TRAINING</a>
+                    <a className="nav-link" href="#contact">CONTACT</a>
+                    <a className="nav-link" href="https://mujaware.com/blog">BLOG</a>
+                </nav>
+            </div>
+            <div>
+                <iframe className="d-block mx-auto" width="640" height="1140" src="https://docs.google.com/forms/d/e/1FAIpQLSeKFnG38GnNPdAnys8R2tNNsqgOhHE8f8LovRHjA3l3j8UTrA/viewform?embedded=true" >Loading…</iframe>
+            </div>
+        </main>
+    )
+
+}
